453b7e7c2dc0fc76aa980c53d0de18df757dbed9
Leonardo <leonardo.vargas29@gmail.com>
2023-03-21 22:23:39 -0300

    benefits section finished

--- fitness-center/src/scenes/benefits/index.tsx
+++ fitness-center/src/scenes/benefits/index.tsx
@@ -1,7 +1,9 @@
+import ActionButton from "@/shared/ActionButton"
 import HText from "@/shared/HText"
 import { BenefitType, SelectedPage } from "@/shared/types"
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid"
 import { motion } from "framer-motion"
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png"
 import Benefit from "./Benefit"
 
 const benefits: Array<BenefitType> = [
@@ -17,10 +19,17 @@ const benefits: Array<BenefitType> = [
   },
   {
     icon: <AcademicCapIcon className="h-6 w-6" />,
-    title: "Expesrt and Pro Trainers",
+    title: "Experts and Pro Trainers",
     description: "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
   },
 ]
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+}
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void
@@ -32,14 +41,30 @@ const Benefits = ({ setSelectedPage }: Props) => {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
       >
-        <div className="md:my-5 md:w-3/5">
+        <motion.div
+          className="md:my-5 md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once:true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: {opacity: 1, x: 0},
+          }}
+        >
           <HText>MORE THAN JUST A GYM.</HText>
           <p className="my-5 text-sm">
             We provide world class fitness equipment, trainers and classes to get you to your ultimate fitness goals with ease. We provide true care into each and every member.
           </p>
-        </div>
+        </motion.div>
 
-        <div className="md:flex items-center justify-between gap-8 mt-5">
+        <motion.div
+          className="md:flex items-center justify-between gap-8 mt-5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.4 }}
+          variants={container}
+        >
           {benefits.map((benefit: BenefitType) => (
             <Benefit 
               key={benefit.title}
@@ -49,7 +74,71 @@ const Benefits = ({ setSelectedPage }: Props) => {
               setSelectedPage={setSelectedPage}
             />
           ))}
+        </motion.div>
+
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+          <img className="mx-auto" alt="working out woman" src={BenefitsPageGraphic}/>
+
+          <div>
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once:true, amount: 0.5 }}
+                  transition={{ duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: 50 },
+                    visible: {opacity: 1, x: 0},
+                  }}
+                >
+                  <HText>
+                    MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                    <span className="text-primary-500">FIT</span>
+                    .
+                  </HText>
+                </motion.div>
+              </div>
+            </div>
+          
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once:true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: {opacity: 1, x: 0},
+              }}
+            >
+              <p className="my-5">
+                Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
+                egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
+                fames vitae vitae quis. Quis amet vulputate tincidunt at in
+                nulla nec. Consequat sed facilisis dui sit egestas ultrices
+                tellus. Ullamcorper arcu id pretium sapien proin integer nisl.
+                Felis orci diam odio.
+              </p>
+              <p className="mb-5">
+                Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+                tellus quam porttitor. Mauris velit euismod elementum arcu neque
+                facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+                enim mattis odio in risus nunc.
+              </p>
+            </motion.div>
+
+            <div className="relative mt-16">
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+
+              </div>
+            </div>
+
+          </div>
         </div>
+
       </motion.div>
     </section>
   )
